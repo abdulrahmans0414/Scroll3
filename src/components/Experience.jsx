18309c4c6a4857fee8cbd969f36e92124a6fbb73
Abdul Rahman <abdulrahmans0414@gmail.com>
2025-03-30 15:21:45 +0530

    add WawaOffice 3D model with scroll animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,34 @@
-import { OrbitControls } from '@react-three/drei' // Note: OrbitControls moved to drei
+import { OrbitControls, ScrollControls } from '@react-three/drei' // Note: OrbitControls moved to drei
+import { Office } from './Office'
 
 function Experience() {
     return (
         <>
-            <OrbitControls />
-            <mesh>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshNormalMaterial />
-            </mesh>
+            {/* Lights (improved setup) */}
+            <ambientLight intensity={0.5} />  {/* Softer ambient light */}
+            <directionalLight
+                position={[10, 10, 10]}
+                intensity={1.5}
+                castShadow  // Enable shadows
+                shadow-mapSize={[1024, 1024]}  // Higher resolution shadows
+            />
+
+            {/* OrbitControls (with smoother settings) */}
+            <OrbitControls
+                enableZoom={false}
+                enablePan={false}  // Disable panning if not needed
+                dampingFactor={0.05}  // Smoother rotation
+            />
+
+            {/* Scroll Controls */}
+            <ScrollControls pages={3} damping={0.25}>
+                {/* Office model with shadow casting */}
+                <Office
+                    castShadow  // Ensure the model casts shadows
+                    receiveShadow  // If it should receive shadows (e.g., floor)
+                />
+            </ScrollControls>
+
         </>
     )
 }
